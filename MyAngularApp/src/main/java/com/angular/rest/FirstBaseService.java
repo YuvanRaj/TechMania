2ff38b8cdefce62351c9e3411d4bcd3ada913d1a
YuvanRaj <yuviram91@gmail.com>
2018-03-12 20:53:42 +0530

    Logger enabled for project

--- MyAngularApp/src/main/java/com/angular/rest/FirstBaseService.java
+++ MyAngularApp/src/main/java/com/angular/rest/FirstBaseService.java
@@ -9,20 +9,24 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.apache.log4j.Logger;
+
 import com.angular.cfc.UserInformation;
 import com.myapp.MysqlRunner;
 
 @Path("/BaseRequest")
 public class FirstBaseService {
-
+	final static Logger logger = Logger.getLogger(FirstBaseService.class);
 	@Path("getName")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public List getUserName() {
+		logger.info("Entered in FirstBaseService getUserName Method");
 		try {
 			UserInformation userInfoObject = null;
 			List userInformationList = new ArrayList(10);
 			MysqlRunner mysql = new MysqlRunner();
+			logger.info("Exit in FirstBaseService getUserName Method");
 			return mysql.getBookingList();
 		} catch (SQLException e) {
 			e.printStackTrace();
